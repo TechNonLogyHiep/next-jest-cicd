696a1bc11464bf31b5f05cd68cf45c9af94abb37
TN Hiep <nobitah2so4@gmail.com>
2025-09-17 10:50:51 +0700

    fix app blog

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -8,6 +8,6 @@ export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
 
-export default function Page({ params }: Params) {
+export default function Page({ params }: { params: { slug: string } }) {
   return <h1>Slug: {params.slug}</h1>;
 }
