c01a667983d5e1e97654849b6e6963c4440d2bb7
TN Hiep <nobitah2so4@gmail.com>
2025-09-17 11:30:54 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -5,3 +5,11 @@ export const metadata = {
 export default function Page() {
   return <h1>Welcome to VTI academy!!!!</h1>;
 }
+
+export async function generateMetadata() {
+  return [
+    { slug: "hello-world" },
+    { slug: "nextjs-cicd" },
+    { slug: "my-second-post" },
+  ];
+}
