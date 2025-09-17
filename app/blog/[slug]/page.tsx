28fd983da99c5c96e20e3e1db6bb6d26604784fe
TN Hiep <nobitah2so4@gmail.com>
2025-09-17 11:39:14 +0700

    Update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,16 @@
-
-
 export async function generateMetadata() {
   return { title: `Post` };
 }
 
 export default function Page() {
   return <h1>Slug: </h1>;
+}
+
+export async function generateStaticParams() {
+  // Ở đây bạn phải trả về toàn bộ slug có thể build
+  return [
+    { slug: "hello-world" },
+    { slug: "nextjs-ci-cd" },
+    { slug: "my-second-post" },
+  ];
 }
